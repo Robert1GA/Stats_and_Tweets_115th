0a96651b938400786c5edca118f3317f49a4d6bc
Robert1GA <roberts.2015@yahoo.co.uk>
2017-03-29 11:33:05 -0700

    playing around with react components

---
+++ src/MyComponent.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class MyComponent extends Component {
+  render() {
+    return (
+      <div className="well">
+        <h1>Title</h1>
+        <p>This is a component</p>
+      </div>
+    );
+  }
+}
+
+module.exports = MyComponent;
